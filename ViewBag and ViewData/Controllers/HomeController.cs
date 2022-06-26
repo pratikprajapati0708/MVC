6c48e1537eb12c0fc00d77a871e60b972f7126a9
pratikprajapati0708 <prajapatipratik292@gmail.com>
2022-06-26 20:49:25 +0530

    First Commit

---
+++ ViewBag and ViewData/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewBag_and_ViewData.Models;
+
+namespace ViewBag_and_ViewData.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // View Bag and View Data is used to pass Data From Controller to View only.It is dynamic type and
+        // We can assign any name to the property .
+      
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.MyCustomProperty = "Pratik";
+            ViewBag.MyList = new List<string>() { "john", "mark", "kim", "rock" };
+            List<Employee> empList = new List<Employee>()
+            {
+                new Employee(){Email = "a@gmail.com", Id=1, Name="John" },
+                new Employee(){Email = "b@gmail.com", Id=2, Name="John1" },
+                new Employee(){Email = "c@gmail.com", Id=3, Name="John2" },
+                new Employee(){Email = "d@gmail.com", Id=4, Name="John3" },
+               
+
+            };
+            ViewBag.MyEmpList = empList;
+            return View();
+        }
+
+
+        //ViewData works as Dictionary
+
+        public ActionResult Demo()
+        {
+            ViewData["My Key"] = "This is value of my Key";
+            ViewData["MyList"] = new List<string>() { "john", "mark", "kim", "rock" };
+            return View();
+        }
+    }
+}
---
+++ ViewBag and ViewData/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewBag_and_ViewData.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
