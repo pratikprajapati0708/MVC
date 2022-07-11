22af54d0f3401871760c88b9618fc7cc707de318
pratikprajapati0708 <prajapatipratik292@gmail.com>
2022-07-12 00:41:49 +0530

    first commit

---
+++ MVCAppwithDb/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyApp.Models;
+using MyApp.Db.DbOperations;
+namespace MVCAppwithDb.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeRepository repository = null; // type is repository 
+        public HomeController()
+        {
+            repository = new EmployeeRepository();
+        }
+        // GET: Home
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = repository.AddEmployee(model); //returns id
+                if (id > 0)
+                {
+                    ModelState.Clear();
+                    ViewBag.Issuccess = "Data Added";
+                }
+            }
+            return View();
+        }
+        public ActionResult GetAllRecords()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyApp.Db/DbOperations/EmployeeRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyApp.Models;
+
+namespace MyApp.Db.DbOperations
+{
+    public class EmployeeRepository
+    {
+        public int AddEmployee(Employee model) // return type is int so need to return integer type
+        {
+            using (var context = new EmployeeDBEntities()) //Create conncetion with database
+            {
+                Employee emp = new Employee() // created new model object of database and set the property coming from database
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    Code = model.Code,
+                };
+                context.Employee.Add(emp); // Which table in ehich data needs to be added
+                context.SaveChanges();
+                return emp.Id; // returned employee id 
+            }
+        }
+
+        public int AddEmployee(EmployeeModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmployeeModel> GetAllEmployees()
+        {
+            using (var context = new EmployeeDBEntities())
+            {
+                var result = context.Employee.Select(x => new EmployeeModel()
+                {
+                    Id = x.Id,
+                    AddressId = x.AddressId,
+                    Code = x.Code,
+                    Email = x.Email,
+                    FirstName=x.FirstName,
+                    LastName=x.LastName
+
+                }).ToList();
+            }
+        }
+    }
+}
