6156acc9122069c3f1891b3fced31b3a582c1cef
pratikprajapati0708 <prajapatipratik292@gmail.com>
2022-07-13 21:53:58 +0530

    First Commit

---
+++ WebApplicationwithJquery/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationwithJquery.Models;
+
+namespace WebApplicationwithJquery.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetAllStudents()
+        {
+            Student std = new Student()
+            {
+                Id = 1,
+                Name = "XYZ",
+                Address = "test"
+
+            };
+
+            var json = JsonConvert.SerializeObject(std);
+            return Json(json,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApplicationwithJquery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationwithJquery.MvcApplication" Language="C#" %>
