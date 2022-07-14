9cee0f054482da3b493b2883c65f2518ef8ef2d1
pratikprajapati0708 <prajapatipratik292@gmail.com>
2022-07-14 23:35:31 +0530

    First Commit

---
+++ AJAXbeginform/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AJAXbeginform.Models;
+
+namespace AJAXbeginform.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Index(Student model)
+        {
+            return Json("true",JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ AJAXbeginform/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AJAXbeginform.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
